dc791e1b215307e24341191e5f495c57697d416a
Rza-Ismayil <rzaismail82@gmail.com>
2022-02-12 18:11:32 +0400

    done

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,31 @@ class XylophoneApp extends StatefulWidget {
 class _XylophoneAppState extends State<XylophoneApp> {
   final player = AudioCache();
 
-  List<ElevatedButton> generateButtons(List colors) {
-    List<ElevatedButton> boxes = [];
+  List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+  List<Expanded> generateButtons() {
+    List<Expanded> boxes = [];
     for (int i = 1; i < 8; i++) {
-      boxes.add(ElevatedButton(
-        onPressed: () => player.play('note$i.wav'),
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(colors[i - 1]),
-        ),
-        child: const SizedBox(
-          height: 80.0,
-          width: 240.0,
+      boxes.add(Expanded(
+        child: TextButton(
+          onPressed: () => player.play('note$i.wav'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(colors[i - 1]),
+          ),
+          child: Text(
+            '.',
+            style: TextStyle(
+              color: colors[i - 1],
+            ),
+          ),
         ),
       ));
     }
@@ -42,20 +56,11 @@ class _XylophoneAppState extends State<XylophoneApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: generateButtons([
-                Colors.red,
-                Colors.orange,
-                Colors.yellow,
-                Colors.green,
-                Colors.teal,
-                Colors.blue,
-                Colors.purple,
-              ]),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: generateButtons(),
           ),
         ),
       ),
